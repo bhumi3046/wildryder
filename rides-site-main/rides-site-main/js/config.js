25fc0c66d84fff3b643713ad62f5671d5c81ec7b
bhumi3046 <143391198+bhumi3046@users.noreply.github.com>
2025-05-05 19:01:38 +0530

    Add files via upload

---
+++ rides-site-main/rides-site-main/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Z7ofjiBqm',  
+        userPoolClientId: '798ruv79h38ud6e95pfgq8a3oi',
+       
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://r93uq6xdol.execute-api.us-east-1.amazonaws.com/devp' 
+    }
+};
